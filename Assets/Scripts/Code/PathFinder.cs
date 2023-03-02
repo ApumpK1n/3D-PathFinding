2c221c443e6c6fe8b75ca88b56ee0c4b6e49a716
ApumpK1n <lsmzng@163.com>
2023-03-02 23:14:03 +0800

    update

---
+++ Assets/Scripts/Code/PathFinder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pumpkin
+{
+    public enum PathfindingAlgorith
+    {
+        AStar,
+        Greedy
+    }
+
+    public class PathFinder
+    {
+        private PathfindingAlgorith algorithm = PathfindingAlgorith.AStar;
+    }
+}
