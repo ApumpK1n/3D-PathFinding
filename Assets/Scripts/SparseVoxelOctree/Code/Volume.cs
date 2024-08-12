fdc42a968138599c1e1cb0452b810a7d3bcc6e82
lis <lis@ct108.com>
2024-08-12 17:48:22 +0800

    1

---
+++ Assets/Scripts/SparseVoxelOctree/Code/Volume.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pumpkin.SVON
+{
+    public class Volume
+    {
+
+        private Vector3 origin;
+        private Vector3 extent;
+
+
+    }
+}
+
+
